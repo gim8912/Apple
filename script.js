7276aafe59ecbf72c676b1a023edd8ea8e7c13c6
gim8912 <gim8912@gmail.com>
2014-05-15 22:05:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ $(document).ready(function(){
 		
 		$("#xVal").html(Math.round(x));
 		$("#sVal").html(shakepoint);
-		$("#aVal").html(applestack);
+		$("#aVal").html(apple);
 
 		var maxX = window.innerWidth - $("#ball").width();
 		var maxY = window.innerHeight - $("#ball").height();
